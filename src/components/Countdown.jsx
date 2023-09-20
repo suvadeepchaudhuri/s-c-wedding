6afb8421d6ccf9d8e9364d37c5fa9efc5f1e86d6
Suvadeep <suvadeep.1905@gmail.com>
2023-09-19 19:54:17 -0500

    Update countdown timer

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-var wed = new Date(2023, 11, 28, 17, 0, 0, 0)
+var wed = new Date(Date.UTC(2023, 11, 28, 11, 30, 0, 0))
 export default function Countdown() {
     const [timeRemaining, setTimeRemaining] = useState({ day: '', hour: '', min: '', sec: '' });
     const calculateTime = () => {
@@ -25,6 +25,6 @@ export default function Countdown() {
     }, [])
 
     return (<div>
-        <span className='w-10 inline-block'>{timeRemaining.day}</span> {timeRemaining.day == 1 ? 'day' : 'days'},  <span className='w-10 inline-block'>{timeRemaining.hour}</span> {timeRemaining.hour == 1 ? 'hour' : 'hours'},  <span className='w-10 inline-block'>{timeRemaining.min}</span> min and <span className='w-10 inline-block'>{timeRemaining.sec}</span> sec to go!
+        <span className='w-15 inline-block'>{timeRemaining.day}</span> <span className='text-2xl'>{timeRemaining.day == 1 ? 'day' : 'days'}</span>,  <span className='w-10 inline-block'>{timeRemaining.hour}</span> <span className='text-2xl'>{timeRemaining.hour == 1 ? 'hour' : 'hours'}</span>,  <span className='w-10 inline-block'>{timeRemaining.min}</span> <span className='text-2xl'>min and</span> <span className='w-10 inline-block'>{timeRemaining.sec}</span> <span className='text-xl'>sec to go!</span>
     </div>);
 }
